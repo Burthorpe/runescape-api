c9fbafe6e4edcbc1214e1c88229eea58031b79f4
Wade Urry <hi@iwader.co.uk>
2013-12-01 15:40:19 +0000

    OldSchool combat calc

--- src/Burthorpe/RunescapeApi/OldSchoolApi.php
+++ src/Burthorpe/RunescapeApi/OldSchoolApi.php
@@ -17,6 +17,19 @@ class OldSchoolApi
     private $skills = array('overall', 'attack', 'defence', 'strength', 'hitpoints', 'ranged', 'prayer', 'magic', 'cooking', 'woodcutting', 'fletching', 'fishing', 'firemaking', 'crafting', 'smithing', 'mining', 'herblore', 'agility', 'thieving', 'slayer', 'farming', 'runecrafting', 'hunter', 'construction');
     
     /**
+     * Default combat stats
+     */
+    private $defaultStats = array(
+        'attack'    => 1,
+        'strength'  => 1,
+        'defence'   => 1,
+        'hitpoints' => 10,
+        'ranged'    => 1,
+        'prayer'    => 1,
+        'magic'     => 1,
+    );
+    
+    /**
      * A CURL wrapper for easily fetching data from a URL
      * 
      * @param string $url The url the curl request should fetch
@@ -77,11 +90,42 @@ class OldSchoolApi
         {
             $stat = explode(",", $result[$i]);
             
-            $stats[$this->skills[$i]]['rank'] = $stat[0];
-            $stats[$this->skills[$i]]['level'] = $stat[1];
-            $stats[$this->skills[$i]]['exp'] = $stat[2];
+            $stats[$this->skills[$i]]['rank'] = (int) $stat[0];
+            $stats[$this->skills[$i]]['level'] = (int) $stat[1];
+            $stats[$this->skills[$i]]['exp'] = (int) $stat[2];
         }
         
         return $stats;
+    }
+    
+    /**
+     * Calculates a users combat level
+     * 
+     * @param array $stats An single level array of the users levels. Only combat levels are used, if missing are assumed the default levels
+     * @return array An array of calculations relating to the users combat level.
+     */
+    public function calcCombat($stats)
+    {
+        $stats = array_merge($this->defaultStats, $stats);
+        
+        $combat['base_level'] = ($stats['defence'] + $stats['hitpoints'] + floor($stats['prayer'] / 2)) * 0.25;
+        $combat['base_melee'] = ($stats['attack'] + $stats['strength']) * 0.325;
+        $combat['base_range'] = floor($stats['ranged'] * 1.5) * 0.325;
+        $combat['base_magic'] = floor($stats['magic'] * 1.5) * 0.325;
+        
+        $combat['combat_level'] = $combat['base_level'] + max($combat['base_melee'], $combat['base_range'], $combat['base_magic']);
+        
+        if (is_float($combat['combat_level']))
+            $combat['remainder_diff'] = ceil($combat['combat_level']) - $combat['combat_level'];
+        else
+            $combat['remainder_diff'] = $combat['combat_level'] + 1;
+        
+        $combat['remainders'] = array(
+            'strength_attack' => ceil($combat['remainder_diff'] / (1.0/3.0)),
+            'defence_constitution' => ceil($combat['remainder_diff'] / (0.25)),
+            'prayer' => ceil($combat['remainder_diff'] / 0.125),
+        );
+        
+        return $combat;
     }
 }
