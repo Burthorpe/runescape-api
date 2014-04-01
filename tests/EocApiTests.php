69265bcb274810ce69362c6bab326eae049124a0
Wade Urry <wader@iwader.co.uk>
2014-04-01 21:25:41 +0100

    Tests for function EocApi->calcCombat() function

---
+++ tests/EocApiTests.php
@@ -0,0 +1,130 @@
+<?php
+
+use Burthorpe\RunescapeApi\EocApi;
+
+class EocApiTests extends PHPUnit_Framework_TestCase {
+
+  /**
+   * EocApi instance
+   *
+   * @var Burthorpe\RunescapeApi\EocApi
+   */
+  protected $EocApi;
+
+  public function setUp()
+  {
+    $this->EocApi = new EocApi;
+  }
+
+  public function testMinimumCalcCombat()
+  {
+    $combat = $this->EocApi->calcCombat([
+        'attack'    => 1,
+        'strength'  => 1,
+        'defence'   => 1,
+        'ranged'    => 1,
+        'magic'     => 1,
+      ]);
+
+    $this->assertEquals(4, $combat['combat_level']);
+  }
+
+  public function testMaximumCalcCombat()
+  {
+    $combat = $this->EocApi->calcCombat([
+        'attack'    => 99,
+        'strength'  => 99,
+        'defence'   => 99,
+        'ranged'    => 99,
+        'magic'     => 99,
+      ]);
+
+    $this->assertEquals(200, $combat['combat_level']);
+  }
+
+  public function testMaxAttackCalcCombat()
+  {
+    $combat = $this->EocApi->calcCombat([
+        'attack'    => 99,
+        'strength'  => 1,
+        'defence'   => 1,
+        'ranged'    => 1,
+        'magic'     => 1,
+      ]);
+
+    $this->assertEquals(102, $combat['combat_level']);
+    $this->assertEquals(102, $combat['attack_combat']);
+    $this->assertEquals(4, $combat['strength_combat']);
+    $this->assertEquals(4, $combat['ranged_combat']);
+    $this->assertEquals(4, $combat['magic_combat']);
+  }
+
+  public function testMaxStrengthCalcCombat()
+  {
+    $combat = $this->EocApi->calcCombat([
+        'attack'    => 1,
+        'strength'  => 99,
+        'defence'   => 1,
+        'ranged'    => 1,
+        'magic'     => 1,
+      ]);
+
+    $this->assertEquals(102, $combat['combat_level']);
+    $this->assertEquals(4, $combat['attack_combat']);
+    $this->assertEquals(102, $combat['strength_combat']);
+    $this->assertEquals(4, $combat['ranged_combat']);
+    $this->assertEquals(4, $combat['magic_combat']);
+  }
+
+  public function testMaxDefenceCalcCombat()
+  {
+    $combat = $this->EocApi->calcCombat([
+        'attack'    => 1,
+        'strength'  => 1,
+        'defence'   => 99,
+        'ranged'    => 1,
+        'magic'     => 1,
+      ]);
+
+    $this->assertEquals(102, $combat['combat_level']);
+    $this->assertEquals(102, $combat['attack_combat']);
+    $this->assertEquals(102, $combat['strength_combat']);
+    $this->assertEquals(102, $combat['ranged_combat']);
+    $this->assertEquals(102, $combat['magic_combat']);
+  }
+
+  public function testMaxRangedCalcCombat()
+  {
+    $combat = $this->EocApi->calcCombat([
+        'attack'    => 1,
+        'strength'  => 1,
+        'defence'   => 1,
+        'ranged'    => 99,
+        'magic'     => 1,
+      ]);
+
+    $this->assertEquals(102, $combat['combat_level']);
+    $this->assertEquals(4, $combat['attack_combat']);
+    $this->assertEquals(4, $combat['strength_combat']);
+    $this->assertEquals(102, $combat['ranged_combat']);
+    $this->assertEquals(4, $combat['magic_combat']);
+  }
+
+  public function testMaxMagicCalcCombat()
+  {
+    $combat = $this->EocApi->calcCombat([
+        'attack'    => 1,
+        'strength'  => 1,
+        'defence'   => 1,
+        'ranged'    => 1,
+        'magic'     => 99,
+      ]);
+
+    $this->assertEquals(102, $combat['combat_level']);
+    $this->assertEquals(4, $combat['attack_combat']);
+    $this->assertEquals(4, $combat['strength_combat']);
+    $this->assertEquals(4, $combat['ranged_combat']);
+    $this->assertEquals(102, $combat['magic_combat']);
+  }
+
+}
